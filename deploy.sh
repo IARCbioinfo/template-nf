47a53d2adefc07776e68e2046c8f84c4bbc00362
Matthieu Foll <follm@iarc.fr>
2017-06-14 15:12:52 +0200

    Create deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+cd ~/template-nf/
+git config --global user.email "follm@iarc.fr"
+git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
+git add .
+git status
+git commit -m "Generated DAG"
+git push origin $CIRCLE_BRANCH
+
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/template-nf/trigger/71022421-2df0-4266-9dfa-cacdd9cbf77b/
