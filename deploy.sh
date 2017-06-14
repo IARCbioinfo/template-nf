f7359190eab2c3709f576d07cdbd0be0b02c817c
Matthieu Foll <follm@iarc.fr>
2017-06-14 15:25:15 +0200

    skip circleCI when generating DAG

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git config --global user.email "follm@iarc.fr"
 git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
 git add .
 git status
-git commit -m "Generated DAG"
+git commit -m "Generated DAG [skip ci]"
 git push origin $CIRCLE_BRANCH
 
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/template-nf/trigger/71022421-2df0-4266-9dfa-cacdd9cbf77b/
